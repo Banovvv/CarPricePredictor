9a14d39f690c8277beec4d9f11b0b4ff17137ad5
Ivan Gechev <ivan_banov@abv.bg>
2023-01-06 15:29:49 +0200

    Basic Car model

---
+++ src/Core/Domain/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Body { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Fuel { get; set; }
+        public string Transmition { get; set; }
+        public decimal Price { get; set; }
+    }
+}
